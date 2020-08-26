5b44e483a95d13ce50e88c41ed7e533d4a7fc387
nikarasu <niklas.utterback@gmail.com>
2020-08-26 11:41:33 +0200

    Added operators to vector2: + += - -=

--- ToolsAndUtilities/ToolsAndUtilities/src/Vector2.h
+++ ToolsAndUtilities/ToolsAndUtilities/src/Vector2.h
@@ -23,9 +23,75 @@ namespace Math
 		}
 
 		//Destructor
-		~Vector2();
+		~Vector2()
+		{
+
+		}
+
+		//Return length of vector
+		//T Length();
+
+		//Return squared length of vector
+		//T SqrLength();
+
+		//Return normalized vector
+		//Vector2<T> GetNormal();
+
+		//Normalize this vector
+		//void Normalize();
+
+		//Get dot product of vector
+		//T GetDotProduct(Vector2<T> aVec);
+
+		//Convert to Vector3
+		//Vector3<T> ToVector3(T aZ);
+
+		//Convert to Vector4
+		//Vector4<T> ToVector4(T aZ, T aW);
+
 
 		T x;
 		T y;
 	};
+
+	//+ operator
+	//Remember: left hand must be first parameter
+	template<typename T>
+	Vector2<T> operator+(const Vector2<T>& aLeftHand, const Vector2<T>& aRightHand)
+	{
+		return Vector2<T>(
+			aRightHand.x + aLeftHand.x,
+			aRightHand.y + aLeftHand.y
+			);
+	}
+
+	//+= operator
+	//Remember: left hand must be first parameter
+	template<typename T>
+	void operator+=(Vector2<T>& aLeftHand, const Vector2<T>& aRightHand)
+	{
+		aLeftHand.x += aRightHand.x;
+		aLeftHand.y += aRightHand.y;
+	}
+
+	//- operator
+	//Remember: left hand must be first parameter
+	template<typename T>
+	Vector2<T> operator-(const Vector2<T>& aLeftHand, const Vector2<T>& aRightHand)
+	{
+		return Vector2<T>(
+			aLeftHand.x - aRightHand.x,
+			aLeftHand.y - aRightHand.y
+			);
+	}
+
+	//-= operator
+	//Remember: left hand must be first parameter
+	template<typename T>
+	void operator-=(Vector2<T>& aLeftHand, const Vector2<T>& aRightHand)
+	{
+		aLeftHand.x -= aRightHand.x;
+		aLeftHand.y -= aRightHand.y;
+	}
+
 }
--- ToolsAndUtilities/Unit Tests/Unit Tests.cpp
+++ ToolsAndUtilities/Unit Tests/Unit Tests.cpp
@@ -37,10 +37,106 @@ namespace UnitTests
 
 				vec1 = vec2;
 
-				assert(vec1.x = 10 && vec1.y == 11);
+				assert(vec1.x == 10 && vec1.y == 11);
 			}
 
-			TEST_METHOD()
+
+			TEST_METHOD(AddVectorIntToVectorInt)
+			{
+				Math::Vector2<int> vec1(7, 23);
+				Math::Vector2<int> vec2(10, 11);
+
+				Math::Vector2<int> vec3 = vec1 + vec2;
+
+				assert(vec3.x == 17 && vec3.y == 34);
+			}
+
+			TEST_METHOD(PlusEqualVectorIntToVectorInt)
+			{
+				Math::Vector2<int> vec1(7, 23);
+				Math::Vector2<int> vec2(10, 11);
+
+				vec1 += vec2;
+
+				assert(vec1.x == 17 && vec1.y == 34);
+			}
+
+			TEST_METHOD(SubtractVectorIntToVectorInt)
+			{
+				Math::Vector2<int> vec1(10, 11);
+				Math::Vector2<int> vec2(7, 23);
+
+				Math::Vector2<int> vec3 = vec1 - vec2;
+
+				assert(vec3.x == 3 && vec3.y == -12);
+			}
+
+			TEST_METHOD(MinusEqualVectorIntToVectorInt)
+			{
+				Math::Vector2<int> vec1(10, 11);
+				Math::Vector2<int> vec2(7, 23);
+
+				vec1 -= vec2;
+
+				assert(vec1.x == 3 && vec1.y == -12);
+			}
+
+			/*
+			TEST_METHOD(MultiplyVector2IntWithScalar)
+			{
+				Math::Vector2<int> vec(7, 23);
+
+				vec = vec * 3;
+
+				assert(vec.x == 21 && vec.y == 69);
+			}
+
+			TEST_METHOD(MultiplyEqualVector2IntWithScalar)
+			{
+				Math::Vector2<int> vec(7, 23);
+
+				vec *= 3;
+
+				assert(vec.x == 21 && vec.y == 69);
+			}
+
+			TEST_METHOD(DivideVector2IntWholeNumbers)
+			{
+				Math::Vector2<int> vec(7, 21);
+
+				vec = vec / 7;
+
+				assert(vec.x == 1 && vec.y == 3);
+			}
+
+			TEST_METHOD(DivideEqualVector2IntWholeNumbers)
+			{
+				Math::Vector2<int> vec(7, 21);
+
+				vec /= 7;
+
+				assert(vec.x == 1 && vec.y == 3);
+			}
+
+			TEST_METHOD(DivideVector2IntOddNumbers)
+			{
+				Math::Vector2<int> vec(9, 20);
+
+				vec = vec / 7;
+
+				assert(vec.x == 1 && vec.y == 2);
+			}
+
+			TEST_METHOD(DivideEqualVector2IntOddNumbers)
+			{
+				Math::Vector2<int> vec(9, 20);
+
+				vec /= 7;
+
+				assert(vec.x == 1 && vec.y == 2);
+			}
+
+			*/
 		};
 	}
 }
